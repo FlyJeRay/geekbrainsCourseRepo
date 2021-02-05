e433da4b46847ef2727e6738e58f517461dc1cb3
Даниил Антонюк <shioto@MacBook-Pro-Daniil.local>
2021-02-05 16:24:52 +0200

    Домашнее задание №2

---
+++ geekbrainsLessonProject/addition.cpp
@@ -0,0 +1,4 @@
+int add_a = 1;
+int add_b = 3;
+int add_c = 5;
+int add_d = 7;
--- geekbrainsLessonProject/main.cpp
+++ geekbrainsLessonProject/main.cpp
@@ -1,45 +1,60 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
-    //
-    // Домашнее задание, часть 1
-    //
+    // ====================================
+    // Домашнее задание №1
+    // ====================================
     
-    short int number = 12;
-    int integer = 583;
-    long long numberTwo = 123'432'765;
-    char character = 'A';
-    bool boolean = true;
-    double p = 3.14;
+    const int a = 1;
+    const int b = 5;
+    const int c = 7;
+    const int d = 9;
     
-    enum symbols { X, O };
-    symbols symbolArray[5] = { X, O, O, X, O };
+    float result = a * (b + (float(c) / d));
+    cout << result << endl;
+        // 5.77778
     
-    //
-    // Домашнее задание, одна звёздочка
-    //
+    // ====================================
+    // Домашнее задание №2
+    // ====================================
     
-    struct ticTacToeField {
-        int fieldHeight; // Высота игрового поля
-        int fieldWidht; // Ширина игрового поля
-        int comboForWin; // Сколько крестиков или ноликов нужно собрать в ряд для победы
+    int number;
+    
+    cout << "Запишите целое число: " << endl;
+    cin >> number;
+    
+    number <= 21 ?
+    cout << to_string(number - 21) << endl :
+    cout << to_string(2 * (number + 21)) << endl;
+    
+    // ====================================
+    // Домашнее задание со звёдочкой
+    // ====================================
+    
+    int array[3][3][3] = {
+        { {1, 2, 3},    {4, 5, 6},    {7, 8, 9} },
+        { {10, 11, 12}, {13, 14, 15}, {16, 17, 18} },
+        { {19, 20, 21}, {22, 23, 24}, {25, 26, 27} }
     };
     
-    //
-    // Домашнее задание, две звёздочки
-    //
+    int * pointer;
+    pointer = & array[1][1][1];
     
-    union testUnion {
-        int unInteger;
-        float unFloat;
-        char unChar;
-    };
+    cout << *pointer << endl;
+        // Вывод - 14
     
-    struct homeworkStructure {
-        testUnion _union;
-        
-        unsigned short isInt : 1;
-        unsigned short isFloat : 1;
-        unsigned short isChar : 1;
-    };
+    // ====================================
+    // Домашнее задание с двумя звёздочками
+    // ====================================
+    
+    extern int add_a;
+    extern int add_b;
+    extern int add_c;
+    extern int add_d;
+    
+    float result_2 = add_a * (add_b + (float(add_c) / add_d));
+    cout << result_2 << endl;
+        // 3.71429
 }
