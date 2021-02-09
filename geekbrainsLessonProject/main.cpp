721aa0219b279bf2f8fc15bf5fd701a3c2ebef18
Даниил Антонюк <shioto@MacBook-Pro-Daniil.local>
2021-02-09 16:56:15 +0200

    Домашнее задание №3

--- geekbrainsLessonProject/main.cpp
+++ geekbrainsLessonProject/main.cpp
@@ -1,60 +1,96 @@
 #include <iostream>
 
 using namespace std;
+bool isLeapYear(int year);
 
 int main(){
-    // ====================================
+    
+    // =============================================
     // Домашнее задание №1
-    // ====================================
+    // =============================================
     
-    const int a = 1;
-    const int b = 5;
-    const int c = 7;
-    const int d = 9;
+    int hw1_number;
     
-    float result = a * (b + (float(c) / d));
-    cout << result << endl;
-        // 5.77778
+    cout << "enter number" << endl;
+    cin >> hw1_number;
     
-    // ====================================
+    if (hw1_number <= 20 && hw1_number >= 10) {
+        cout << "hw1-true" << endl;
+    }
+    else {
+        cout << "hw1-false" << endl;
+    }
+    
+    // =============================================
     // Домашнее задание №2
-    // ====================================
+    // =============================================
     
-    int number;
+    const int hw2_number_1 = 5;
+    const int hw2_number_2 = 5;
     
-    cout << "Запишите целое число: " << endl;
-    cin >> number;
+    if ((hw2_number_1 == 10 && hw2_number_2 == 10) || hw2_number_1 + hw2_number_2 == 10) {
+        cout << "hw2-true" << endl;
+    }
+    else {
+        cout << "hw2-false" << endl;
+    }
     
-    number <= 21 ?
-    cout << to_string(number - 21) << endl :
-    cout << to_string(2 * (number + 21)) << endl;
+    // =============================================
+    // Домашнее задание №3
+    // =============================================
     
-    // ====================================
-    // Домашнее задание со звёдочкой
-    // ====================================
+    for (int i = 0; i < 50; i++) {
+        if (i % 2 != 0) {
+            cout << to_string(i) + " ";
+        }
+    }
     
-    int array[3][3][3] = {
-        { {1, 2, 3},    {4, 5, 6},    {7, 8, 9} },
-        { {10, 11, 12}, {13, 14, 15}, {16, 17, 18} },
-        { {19, 20, 21}, {22, 23, 24}, {25, 26, 27} }
-    };
+    cout << endl;
     
-    int * pointer;
-    pointer = & array[1][1][1];
+    // =============================================
+    // Домашнее задание со звёздочкой
+    // =============================================
     
-    cout << *pointer << endl;
-        // Вывод - 14
+    // Результат. Изначально стоит true и *если* окажется,
+    // что число не простое, изменится на false
+    bool hwStar_result = true;
+    int hwStar_number;
     
-    // ====================================
+    cout << "enter number" << endl;
+    cin >> hwStar_number;
+    
+    for (int i = 1; i < hwStar_number; i++) {
+        if (i != 1 && i != hwStar_number) {
+            if (hwStar_number % i == 0) hwStar_result = false;
+        }
+    }
+    
+    if (hwStar_result) cout << "hwStar-true" << endl;
+    else cout << "hwStar-false" << endl;
+    
+    // =============================================
     // Домашнее задание с двумя звёздочками
-    // ====================================
+    // =============================================
     
-    extern int add_a;
-    extern int add_b;
-    extern int add_c;
-    extern int add_d;
+    int hw2Star_year;
+    cout << "write the year" << endl;
+    cin >> hw2Star_year;
     
-    float result_2 = add_a * (add_b + (float(add_c) / add_d));
-    cout << result_2 << endl;
-        // 3.71429
+    if (hw2Star_year < 1 || hw2Star_year > 3000) {
+        cout << "invalid year" << endl;
+    }
+    
+    // Использование функции в нижней части кода (строки 90-99)
+    bool hw2Star_result = isLeapYear(hw2Star_year);
+    
+    if (hw2Star_result) cout << "hw2Star-true" << endl;
+    else cout << "hw2Star-false" << endl;
+}
+
+// Функция для домашнего задания с двумя звёздочками
+bool isLeapYear(int year) {
+    if (year % 400 == 0) return true;
+    else if (year % 100 == 0) return false;
+    else if (year % 4 == 0) return true;
+    else return false;
 }
