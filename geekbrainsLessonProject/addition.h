a4cbff54946fd11a2d92ba8b00163015d1adf3f1
FlyJeRay <flyjeray.dev@gmail.com>
2021-03-05 14:18:03 +0200

    Удаление ненужных файлов

--- geekbrainsLessonProject/addition.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace myNameSpace {
-    float * task1_initializeArray(float array[], int arraySize);
-    void task1_printArray(float array[], int arraySize);
-    void task1_sortArray(float array[], int arraySize);
-}
